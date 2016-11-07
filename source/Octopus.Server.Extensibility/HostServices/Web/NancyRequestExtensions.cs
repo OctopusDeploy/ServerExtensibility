f0b0c6a6a35c7fec9ac6cf73f4010cf0103c9220
Shannon Lewis <slewis74@gmail.com>
2016-11-07 10:14:03 +1000

    Added extension method for determining the Nancy request virtual directory path.

---
+++ source/Octopus.Server.Extensibility/HostServices/Web/NancyRequestExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+
+namespace Octopus.Server.Extensibility.HostServices.Web
+{
+    public static class NancyRequestExtensions
+    {
+        /// <summary>
+        /// Builds the path to the virtual directory for the given Request.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>The full virtual directory path.</returns>
+        public static string DirectoryPath(this Request request)
+        {
+            return request.Url.SiteBase + request.Url.BasePath;
+        }
+    }
+}
