a896736d568b8c3240bcf9d64b46918a6b58102a
Shannon Lewis <slewis74@gmail.com>
2016-11-11 09:05:28 +1000

    Changed return type so we can explain if there are configuration issues with the received headers

--- source/Octopus.Server.Extensibility/HostServices/Web/NancyRequestExtensions.cs
+++ source/Octopus.Server.Extensibility/HostServices/Web/NancyRequestExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Nancy;
 
 namespace Octopus.Server.Extensibility.HostServices.Web
@@ -6,11 +7,16 @@ namespace Octopus.Server.Extensibility.HostServices.Web
     public static class NancyRequestExtensions
     {
         /// <summary>
-        /// Builds the path to the virtual directory for the given Request.
+        /// Builds the path to the virtual directory for the given Request.  This is required in scenarios where the server 
+        /// needs to understand the absolute path it was called on, from the browser's perspecive.  This is essential to
+        /// making things like the OpenID Connect login process work, because we have to provide them a redirectUrl that will
+        /// get them back to our API using the same protocol and host that the original call was made on (remembering that there
+        /// could be multiple listening prefixes at play and we could also be behind a reverse proxy).
         /// </summary>
         /// <param name="request">The request.</param>
-        /// <returns>The full virtual directory path.</returns>
-        public static string DirectoryPath(this Request request)
+        /// <returns>A result representing full virtual directory path, if it could be determined; otherwise a result explaining 
+        /// why it couldn't be determined.</returns>
+        public static DirectoryPathResult DirectoryPath(this Request request)
         {
             var urlSiteBase = request.Url.SiteBase;
 
@@ -19,10 +25,13 @@ namespace Octopus.Server.Extensibility.HostServices.Web
             {
                 var forwardedProto = request.Headers["X-Forwarded-Proto"].FirstOrDefault();
 
+                if (forwardedProto == null)
+                    return DirectoryPathResult.Invalid("X-Forwarded-Host' was supplied, but 'X-Forwarded-Proto' header was not. Please configure your proxy to pass this header through.");
+
                 urlSiteBase = $"{forwardedProto}://{forwardedHost}";
             }
 
-            return urlSiteBase + request.Url.BasePath;
+            return DirectoryPathResult.Success(urlSiteBase + request.Url.BasePath);
         }
     }
 }
