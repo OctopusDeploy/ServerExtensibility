7b9cc7ce826cc95181c9204b76471e87ae01df13
Shannon Lewis <slewis74@gmail.com>
2016-11-30 09:39:31 +1000

    Added Obsolete.Fody to mark methods in the CSS and Javascript contributor interfaces as obsolete

--- source/Octopus.Server.Extensibility/Extensions/Infrastructure/Web/Content/IContributesJavascript.cs
+++ source/Octopus.Server.Extensibility/Extensions/Infrastructure/Web/Content/IContributesJavascript.cs
@@ -4,6 +4,9 @@ namespace Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Content
 {
     public interface IContributesJavascript
     {
+        IEnumerable<string> GetJavascriptUris();
+
+        [ObsoleteEx(RemoveInVersion = "2.0", TreatAsErrorFromVersion = "2.0", ReplacementTypeOrMember = "GetJavascriptUris")]
         IEnumerable<string> GetJavascriptUris(string requestDirectoryPath);
     }
 }
