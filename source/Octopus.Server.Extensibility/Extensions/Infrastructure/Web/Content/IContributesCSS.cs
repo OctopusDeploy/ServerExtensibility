bb573fbe5f82de05899311a2f065532c98077530
Shannon Lewis <slewis74@gmail.com>
2016-11-30 15:26:24 +1000

    Removed the obsolete methods, we're going to 2.0

--- source/Octopus.Server.Extensibility/Extensions/Infrastructure/Web/Content/IContributesCSS.cs
+++ source/Octopus.Server.Extensibility/Extensions/Infrastructure/Web/Content/IContributesCSS.cs
@@ -6,8 +6,5 @@ namespace Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Content
     public interface IContributesCSS
     {
         IEnumerable<string> GetCSSUris();
-
-        [ObsoleteEx(RemoveInVersion = "2.0", TreatAsErrorFromVersion = "2.0", ReplacementTypeOrMember = "GetJavascriptUris")]
-        IEnumerable<string> GetCSSUris(string requestDirectoryPath);
     }
 }
--- source/Octopus.Server.Extensibility/Extensions/Infrastructure/Web/Content/IContributesJavascript.cs
+++ source/Octopus.Server.Extensibility/Extensions/Infrastructure/Web/Content/IContributesJavascript.cs
@@ -5,8 +5,5 @@ namespace Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Content
     public interface IContributesJavascript
     {
         IEnumerable<string> GetJavascriptUris();
-
-        [ObsoleteEx(RemoveInVersion = "2.0", TreatAsErrorFromVersion = "2.0", ReplacementTypeOrMember = "GetJavascriptUris")]
-        IEnumerable<string> GetJavascriptUris(string requestDirectoryPath);
     }
 }
