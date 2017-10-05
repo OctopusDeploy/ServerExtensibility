959ab7537fa5f66ee14ec755d7f624cd84d5f4b3
Andrew Katsivas <andrew.katsivas@gmail.com>
2017-10-05 16:18:20 +1030

    Adjusted the IHasConfigurationSettings to include additional properties for metadata generation

--- source/Node.Extensibility/Extensions/Infrastructure/Configuration/IHasConfigurationSettings.cs
+++ source/Node.Extensibility/Extensions/Infrastructure/Configuration/IHasConfigurationSettings.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Octopus.Node.Extensibility.Extensions.Infrastructure.Configuration
 {
     public interface IHasConfigurationSettings
     {
+        string Id { get; }
+
         string ConfigurationSetName { get; }
+
+        string Description { get; }
+
+        Type MetadataResourceType { get; }
+
         IEnumerable<ConfigurationValue> GetConfigurationValues();
     }
 }
