7aa330f3a591c23edbc81d2c086b5c3891ca9811
Andrew Katsivas <andrew.katsivas@gmail.com>
2017-10-25 21:27:14 +1030

    Finished extracting IResourceMapping from OD

--- source/Node.Extensibility/Extensions/Infrastructure/Configuration/Mapping/IResourceMapping.cs
+++ source/Node.Extensibility/Extensions/Infrastructure/Configuration/Mapping/IResourceMapping.cs
@@ -10,8 +10,20 @@ namespace Octopus.Node.Extensibility.Extensions.Infrastructure.Configuration
     public interface IResourceMapping<TResource, TModel, TContext> : IResourceMapping
         where TResource : IResource
     {
+        IResourceMapping<TResource, TModel, TContext> EnrichResource(Action<TModel, TResource, TContext> callback);
+
+        IResourceMapping<TResource, TModel, TContext> EnrichResource(Action<TModel, TResource> callback);
+
+        IResourceMapping<TResource, TModel, TContext> EnrichModel(Action<TModel, TResource> callback);
+
+        IResourceMapping<TResource, TModel, TContext> EnrichModel(Action<TModel, TResource, TContext> callback);
+
         IResourceMapping<TResource, TModel, TContext> DoNotMap<TProperty>(Expression<Func<TModel, TProperty>> propertyAccessor);
 
-        //enrich
+        IResourceMapping<TResource, TModel, TContext> AllowReuseExisting<TProperty>(
+            Expression<Func<TModel, TProperty>> propertyAccessor);
+
+        IResourceMapping<TResource, TModel, TContext> IncludeSubType<TResourceSub, TModelSub>(Action<IResourceMapping<TResourceSub, TModelSub, TContext>> config = null)
+            where TResourceSub : class, TResource where TModelSub : class, TModel;
     }
 }
