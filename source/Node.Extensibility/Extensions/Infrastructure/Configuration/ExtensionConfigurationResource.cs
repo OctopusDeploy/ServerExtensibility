39f364cd3860ab5d9be948a24f827c051019dee6
Andrew Katsivas <andrew.katsivas@gmail.com>
2017-10-27 10:07:26 +1030

    Added base ExtensionConfigurationResource for all extension resources to inherit from

---
+++ source/Node.Extensibility/Extensions/Infrastructure/Configuration/ExtensionConfigurationResource.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Octopus.Data.Model;
+using Octopus.Data.Resources;
+using Octopus.Data.Resources.Attributes;
+
+namespace Octopus.Node.Extensibility.Extensions.Infrastructure.Configuration
+{
+    public abstract class ExtensionConfigurationResource: IResource
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        [DisplayName("Author")]
+        [Description("The author of this extension")]
+        public string ExtensionAuthor { get; set; }
+
+        [DisplayName("Configuration Schema Version")]
+        [Description("The schema version of this configuration document")]
+        public string ConfigurationSchemaVersion { get; set; }
+
+        [DisplayName("Is Enabled")]
+        [Description("Whether or not this extension is enabled")]
+        [Required]
+        [Writeable]
+        public bool IsEnabled { get; set; }
+
+        public LinkCollection Links { get; set; }
+    }
+}
