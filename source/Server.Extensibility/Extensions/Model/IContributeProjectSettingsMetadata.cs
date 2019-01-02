f098be34f1f58d3b4fd2261cfd4f24934351b7f3
Shannon Lewis <slewis74@gmail.com>
2019-01-02 16:31:19 +1000

    added extension name to the metadata, for displaying on the project settings

--- source/Server.Extensibility/Extensions/Model/IContributeProjectSettingsMetadata.cs
+++ source/Server.Extensibility/Extensions/Model/IContributeProjectSettingsMetadata.cs
@@ -6,6 +6,8 @@ namespace Octopus.Server.Extensibility.Extensions.Model
     public interface IContributeProjectSettingsMetadata
     {
         string ExtensionId { get; }
+        string ExtensionName { get; }
+
         List<PropertyMetadata> Properties { get; }
     }
 }
