8a1a6621bb4f917df35f736d39bbb6005e96dbfc
Shannon Lewis <slewis74@gmail.com>
2019-01-08 15:31:48 +1000

    moving an interface to the IssueTrackerExtensibility package

--- source/Server.Extensibility/Extensions/Model/Projects/IContributeWorkItemsToReleases.cs
+++
@@ -1,9 +0,0 @@
-﻿using Octopus.Server.Extensibility.Resources;
-
-namespace Octopus.Server.Extensibility.Extensions.Model.Projects
-{
-    public interface IContributeWorkItemsToReleases
-    {
-        DeploymentActionPackageResource GetDeploymentAction(string projectId);
-    }
-}
