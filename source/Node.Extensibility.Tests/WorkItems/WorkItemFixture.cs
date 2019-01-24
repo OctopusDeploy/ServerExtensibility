37dc08d917f275e010af16ba75c6fd1f2d368374
Shannon Lewis <slewis74@gmail.com>
2019-01-24 12:02:06 +1000

    added test to make sure we don't ever break the Distinct handling

---
+++ source/Node.Extensibility.Tests/WorkItems/WorkItemFixture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Octopus.Server.Extensibility.HostServices.Model.IssueTrackers;
+
+namespace Node.Extensibility.Tests.WorkItems
+{
+    [TestFixture]
+    public class WorkItemFixture
+    {
+        [Test]
+        public void DistinctWorkItemsBehavesCorrectly()
+        {
+            var listWithDupes = new List<WorkItem>
+            {
+                new WorkItem { Id = "test1" },
+                new WorkItem { Id = "test2" },
+                new WorkItem { Id = "test1" },
+            };
+
+            var distinctWorkItems = listWithDupes.Distinct();
+
+            Assert.AreEqual(2, distinctWorkItems.Count());
+        }
+    }
+}
