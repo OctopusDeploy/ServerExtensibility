e4842dbcf508c346e509a5125afbd2359d1f777f
Shannon Lewis <slewis74@gmail.com>
2019-01-31 16:40:08 +1000

    setting up so DeploymentEnvironments can have extensible settings

---
+++ source/Server.Extensibility/Extensions/Model/Environments/IContributeDeploymentEnvironmentSettingsMetadata.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Octopus.Server.Extensibility.Metadata;
+
+namespace Octopus.Server.Extensibility.Extensions.Model.Environments
+{
+    public interface IContributeDeploymentEnvironmentSettingsMetadata
+    {
+        string ExtensionId { get; }
+        string ExtensionName { get; }
+
+        List<PropertyMetadata> Properties { get; }
+    }
+}
