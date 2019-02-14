725264ccf891f283ae0df40bd5964a333b144b89
Shannon Lewis <slewis74@gmail.com>
2019-02-14 13:49:50 +1000

    added IServerTaskStore

---
+++ source/Server.Extensibility/HostServices/Domain/ServerTasks/IServerTaskStore.cs
@@ -0,0 +1,8 @@
+﻿using Octopus.Server.Extensibility.HostServices.Model.ServerTasks;
+
+namespace Octopus.Server.Extensibility.HostServices.Domain.ServerTasks
+{
+    public interface IServerTaskStore : IStore<IServerTask>
+    {
+    }
+}
---
+++ source/Server.Extensibility/HostServices/Model/ServerTasks/IServerTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Nevermore.Contracts;
+using Octopus.Server.Extensibility.Resources;
+
+namespace Octopus.Server.Extensibility.HostServices.Model.ServerTasks
+{
+    public interface IServerTask : IDocument, IHaveSpace
+    {
+        string Description { get; set; }
+        DateTimeOffset QueueTime { get; set; }
+        TimeSpan QueueTimeExpiry { get; set; }
+        TimeSpan ExecutionTimeExpiry { get; set; }
+        DateTimeOffset? StartTime { get; set; }
+        DateTimeOffset? LastUpdatedTime { get; set; }
+        DateTimeOffset? CompletedTime { get; set; }
+        string ErrorMessage { get; set; }
+        string ConcurrencyTag { get; set; }
+        TaskState State { get; set; }
+        bool HasPendingInterruptions { get; set; }
+        bool HasWarningsOrErrors { get; set; }
+        string ServerVersion { get; set; }
+        string ServerNodeId { get; set; }
+        string LogDifferentiationToken { get; set; }
+        string AdditionalDescription { get; set; }
+        int DurationSeconds { get; set; }
+        int ChildTaskCount { get; set; }
+    }
+}
