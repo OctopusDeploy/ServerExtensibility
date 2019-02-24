b54e172426507d269483b9b1792154dc47b6edc7
Shannon Lewis <slewis74@gmail.com>
2019-02-25 09:05:48 +1000

    moving another interface out of server solution

---
+++ source/Server.Extensibility/HostServices/Model/IPackageReference.cs
@@ -0,0 +1,11 @@
+﻿namespace Octopus.Server.Extensibility.HostServices.Model
+{
+    /// <summary>
+    /// Represents a reference from a deployment-action to a package.
+    /// </summary>
+    public interface IPackageReference
+    {
+        string PackageId { get; }
+        string FeedId { get; }
+    }
+}
