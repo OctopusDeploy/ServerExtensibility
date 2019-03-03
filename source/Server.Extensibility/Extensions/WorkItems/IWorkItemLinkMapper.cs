d80e2bae9cae0eda30491786dcb53cec08a85b47
Shannon Lewis <slewis74@gmail.com>
2019-03-03 14:36:05 +1000

    interface for extensions to map WorkItem to WorkItemLink

---
+++ source/Server.Extensibility/Extensions/WorkItems/IWorkItemLinkMapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Octopus.Server.Extensibility.HostServices.Model.IssueTrackers;
+using Octopus.Server.Extensibility.HostServices.Model.PackageMetadata;
+using Octopus.Server.Extensibility.Resources.IssueTrackers;
+
+namespace Octopus.Server.Extensibility.Extensions.WorkItems
+{
+    public interface IWorkItemLinkMapper
+    {
+        string IssueTrackerId { get; }
+
+        WorkItemLink[] Map(OctopusPackageMetadata packageMetadata, IEnumerable<WorkItem> workItems);
+    }
+}
