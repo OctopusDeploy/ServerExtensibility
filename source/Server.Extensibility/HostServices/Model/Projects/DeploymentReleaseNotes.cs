0ca8cd927807329d52a3ac8f1317374619fc09e6
Shannon Lewis <slewis74@gmail.com>
2019-03-11 22:10:54 +1000

    property initializer

--- source/Server.Extensibility/HostServices/Model/Projects/DeploymentReleaseNotes.cs
+++ source/Server.Extensibility/HostServices/Model/Projects/DeploymentReleaseNotes.cs
@@ -4,6 +4,11 @@ namespace Octopus.Server.Extensibility.HostServices.Model.Projects
 {
     public class DeploymentReleaseNotes
     {
+        public DeploymentReleaseNotes()
+        {
+            VersionMetadata = new OctopusPackageVersionMetadata[0];
+        }
+
         public string Version { get; set; }
         public string ReleaseNotes { get; set; }
         public OctopusPackageVersionMetadata[] VersionMetadata { get; set; }
