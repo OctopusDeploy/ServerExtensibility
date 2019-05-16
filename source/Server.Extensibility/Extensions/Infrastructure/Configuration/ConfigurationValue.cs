0a7a468094761c8b96ae34e4a691e9a7d725d583
Roy Wallace <roy.wallace@octopus.com>
2019-05-16 15:00:59 +1000

    Serialize configuration enum values as strings (#16)

--- source/Server.Extensibility/Extensions/Infrastructure/Configuration/ConfigurationValue.cs
+++ source/Server.Extensibility/Extensions/Infrastructure/Configuration/ConfigurationValue.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Octopus.Server.Extensibility.Extensions.Infrastructure.Configuration
 {
@@ -26,9 +27,13 @@ namespace Octopus.Server.Extensibility.Extensions.Infrastructure.Configuration
         }
 
         public string Key { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
         public object Value => TypedValue;
+
         [JsonIgnore]
         public T TypedValue { get; set; }
+
         public bool ShowInPortalSummary { get; set; }
         public string Description { get; set; }
         public bool IsSensitive { get; set; }
