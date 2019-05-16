bef368d10cac1c0da7297a9a26f11a1dbf30495d
Roy Wallace <waldo000000@gmail.com>
2019-05-16 13:20:01 +1000

    Serialize configuration enum values as strings

---
+++ source/Node.Extensibility.Tests/Extensions/Infrastructure/Configuration/ConfigurationValueFixture.cs
@@ -0,0 +1,26 @@
+using FluentAssertions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Octopus.Server.Extensibility.Extensions.Infrastructure.Configuration;
+
+namespace Node.Extensibility.Tests.Extensions.Infrastructure.Configuration
+{
+    [TestFixture]
+    public class ConfigurationValueFixture
+    {
+        [Test]
+        public void SerializeObject_WithEnumValue_AsString()
+        {
+            var toSerialize = new ConfigurationValue<SomeEnum>("Some.Key", SomeEnum.SomeEnumValue, true, "Some description");
+
+            var serialized = JsonConvert.SerializeObject(toSerialize);
+
+            serialized.Should().Be("{\"Key\":\"Some.Key\",\"Value\":\"SomeEnumValue\",\"ShowInPortalSummary\":true,\"Description\":\"Some description\",\"IsSensitive\":false}");
+        }
+
+        private enum SomeEnum
+        {
+            SomeEnumValue
+        }
+    }
+}
