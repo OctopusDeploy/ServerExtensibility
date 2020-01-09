e47333997be9ab2226bd5a35c67673aa16660c5d
Henrik Andersson <henrik.andersson@octopus.com>
2020-01-09 12:29:04 +1000

    Add a property to get the HttpClientHandler as GitHubIssueTracker needs access to it due to Octokit

--- source/Server.Extensibility/Extensions/Infrastructure/Web/Api/IOctopusHttpClientFactory.cs
+++ source/Server.Extensibility/Extensions/Infrastructure/Web/Api/IOctopusHttpClientFactory.cs
@@ -5,6 +5,7 @@ namespace Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api
 {
     public interface IOctopusHttpClientFactory
     {
+        HttpClientHandler HttpClientHandler { get; }
         HttpClient CreateClient();
     }
 }
