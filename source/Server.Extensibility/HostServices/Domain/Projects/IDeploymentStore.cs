a4be3293436f28385fe27f280c4a8aff58d04c80
Shannon Lewis <slewis74@gmail.com>
2020-03-20 09:12:43 +1000

    Added IDeploymentStore

---
+++ source/Server.Extensibility/HostServices/Domain/Projects/IDeploymentStore.cs
@@ -0,0 +1,8 @@
+using Octopus.Server.Extensibility.HostServices.Model.Projects;
+
+namespace Octopus.Server.Extensibility.HostServices.Domain.Projects
+{
+    public interface IDeploymentStore : IStore<IDeployment>
+    {
+    }
+}
