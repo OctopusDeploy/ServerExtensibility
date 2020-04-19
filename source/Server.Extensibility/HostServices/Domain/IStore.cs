e9717cac8873256c124234714fff692896ffee05
Shannon Lewis <slewis74@gmail.com>
2020-04-19 14:12:59 +1000

    IStore can use an interface, so generics constraints can't be class

--- source/Server.Extensibility/HostServices/Domain/IStore.cs
+++ source/Server.Extensibility/HostServices/Domain/IStore.cs
@@ -1,7 +1,6 @@
 ﻿namespace Octopus.Server.Extensibility.HostServices.Domain
 {
     public interface IStore<out TDocument>
-        where TDocument : class
     {
         TDocument Get(string id);
     }
