b2f9bbb64a9faec2e569c233ef877916c9791c10
Andrew Harcourt <andrew.harcourt@octopus.com>
2020-05-19 13:41:55 +1000

    More input validation on value parameters. If we receive one of these things, it _must_ be able to be assumed that it's valid.

--- source/Server.Extensibility/HostServices/Model/WorkerPools/WorkerPoolName.cs
+++ source/Server.Extensibility/HostServices/Model/WorkerPools/WorkerPoolName.cs
@@ -1,9 +1,15 @@
-﻿namespace Octopus.Server.Extensibility.HostServices.Model.WorkerPools
+﻿using System;
+
+namespace Octopus.Server.Extensibility.HostServices.Model.WorkerPools
 {
-    public class WorkerPoolName : TinyType<string>
+    public class WorkerPoolName : CaseInsensitiveStringTinyType
     {
         public WorkerPoolName(string value) : base(value)
         {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException(nameof(value),
+                    "Worker pool IDs or Names should never be null or empty. If the value you want to communicate is null/empty/whitespace then " +
+                    "just pass a null rather than a tiny type wrapping it.");
         }
     }
 }
