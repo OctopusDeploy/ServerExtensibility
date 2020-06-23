c8f27ff279f44059fa47576be78da830a2d62e24
Shannon Lewis <slewis74@gmail.com>
2020-06-23 11:43:00 +1000

    extras for the ResultFromExtension

--- source/Server.Extensibility/Results/ResultFromExtension.cs
+++ source/Server.Extensibility/Results/ResultFromExtension.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Octopus.Data;
 
 namespace Octopus.Server.Extensibility.Results
@@ -14,6 +16,29 @@ namespace Octopus.Server.Extensibility.Results
         {
             return new ResultFromExtension<T> { ExtensionIsDisabled = true };
         }
+        public static ResultFromExtension<T> Success(T value)
+        {
+            return new ResultFromExtension<T>() { WasSuccessful = true, Value = value };
+        }
+        
+        public static ResultFromExtension<T> Failed()
+        {
+            return new ResultFromExtension<T>() { Errors = new string[0] };
+        }
 
+        public static ResultFromExtension<T> Failed(params string[] errors)
+        {
+            return new ResultFromExtension<T>() { Errors = errors.ToArray() };
+        }
+
+        public static ResultFromExtension<T> Failed(params IResult[] becauseOf)
+        {
+            return new ResultFromExtension<T>() { Errors = becauseOf.Where(s => s.WasFailure).SelectMany(b => b.Errors).ToArray() };
+        }
+
+        public static ResultFromExtension<T> Failed(IReadOnlyCollection<IResult> becauseOf)
+        {
+            return new ResultFromExtension<T>() { Errors = becauseOf.Where(s => s.WasFailure).SelectMany(b => b.Errors).ToArray() };
+        }
     }
 }
