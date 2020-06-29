6ccedc63e8950b93a47efd52968bdd3b20eb3df7
Shannon Lewis <slewis74@gmail.com>
2020-06-29 15:30:08 +1000

    Values for the ExtensionSettingsValues should be nullable

--- source/Server.Extensibility/HostServices/Model/ExtensionSettingsValues.cs
+++ source/Server.Extensibility/HostServices/Model/ExtensionSettingsValues.cs
@@ -5,6 +5,6 @@ namespace Octopus.Server.Extensibility.HostServices.Model
     public class ExtensionSettingsValues
     {
         public string ExtensionId { get; set; } = string.Empty;
-        public object Values { get; set; } = string.Empty;
+        public object? Values { get; set; }
     }
 }
