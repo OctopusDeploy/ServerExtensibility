9304c19d58b5e176757efb2c99ee6c445cf72fa4
Frank Lin <frank.lin@octopus.com>
2020-07-27 11:44:03 +1000

    Support multiple typed parameters more easily

--- source/Server.Extensibility/Extensions/Infrastructure/Web/Api/IOctoRequest.cs
+++ source/Server.Extensibility/Extensions/Infrastructure/Web/Api/IOctoRequest.cs
@@ -17,8 +17,33 @@ namespace Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api
         IDictionary<string, string> Form { get; }
         IDictionary<string, string> Cookies { get; }
         IPrincipal User { get; }
-        Task<IOctoResponseProvider> GetParameterValue<T>(IResponderParameter<T> parameter, Func<T, Task<IOctoResponseProvider>> onSuccess);
-        RequestExecutorBuilder<TParameterType> WithParameter<TParameterType>(IResponderParameter<TParameterType> parameter);
+        Task<IOctoResponseProvider> HandleAsync(
+            Func<Task<IOctoResponseProvider>> onSuccess);
+        Task<IOctoResponseProvider> HandleAsync<T>(
+            IResponderParameter<T> parameter,
+            Func<T, Task<IOctoResponseProvider>> onSuccess);
+        Task<IOctoResponseProvider> HandleAsync<T1, T2>(
+            IResponderParameter<T1> parameter1,
+            IResponderParameter<T2> parameter2,
+            Func<T1, T2, Task<IOctoResponseProvider>> onSuccess);
+        Task<IOctoResponseProvider> HandleAsync<T1, T2, T3>(
+            IResponderParameter<T1> parameter1,
+            IResponderParameter<T2> parameter2,
+            IResponderParameter<T3> parameter3,
+            Func<T1, T2, T3, Task<IOctoResponseProvider>> onSuccess);
+        Task<IOctoResponseProvider> HandleAsync<T1, T2, T3, T4>(
+            IResponderParameter<T1> parameter1,
+            IResponderParameter<T2> parameter2,
+            IResponderParameter<T3> parameter3,
+            IResponderParameter<T4> parameter4,
+            Func<T1, T2, T3, T4, Task<IOctoResponseProvider>> onSuccess);
+        Task<IOctoResponseProvider> HandleAsync<T1, T2, T3, T4, T5>(
+            IResponderParameter<T1> parameter1,
+            IResponderParameter<T2> parameter2,
+            IResponderParameter<T3> parameter3,
+            IResponderParameter<T4> parameter4,
+            IResponderParameter<T5> parameter5,
+            Func<T1, T2, T3, T4, T5, Task<IOctoResponseProvider>> onSuccess);
         TResource GetBody<TResource>(RequestBodyRegistration<TResource> requestBodyRegistration);
     }
 }
