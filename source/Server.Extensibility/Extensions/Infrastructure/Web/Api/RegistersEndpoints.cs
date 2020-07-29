3b2069981ded3a77287fea88a14df091377f536f
Frank Lin <frank.lin@octopus.com>
2020-07-29 16:43:02 +1000

    Remove handler, we should use types

--- source/Server.Extensibility/Extensions/Infrastructure/Web/Api/RegistersEndpoints.cs
+++ source/Server.Extensibility/Extensions/Infrastructure/Web/Api/RegistersEndpoints.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api
 {
     public abstract class RegistersEndpoints
     {
         public List<EndpointRegistration> Registrations { get; } = new List<EndpointRegistration>();
-
-        protected void Add(string method, string path, Func<IOctoRequest, Task<IOctoResponseProvider>> handler)
-        {
-            Registrations.Add(new EndpointRegistration(method, path, handler));
-        }
 
         protected void Add<TInvoker>(string method, string path)
             where TInvoker : IAsyncActionInvoker
@@ -21,31 +15,24 @@ namespace Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api
 
         protected void Add(string method, string path, Type invokerType)
         {
+            if (!typeof(IAsyncActionInvoker).IsAssignableFrom(invokerType))
+                throw new ArgumentException($"The invoker type must implement {nameof(IAsyncActionInvoker)}", nameof(invokerType));
+
             Registrations.Add(new EndpointRegistration(method, path, invokerType));
         }
 
         public class EndpointRegistration
         {
-            public EndpointRegistration(string method, string path, Func<IOctoRequest, Task<IOctoResponseProvider>> handler)
-            {
-                Method = method;
-                Path = path;
-                Handler = handler;
-                InvokerType = null;
-            }
-
             public EndpointRegistration(string method, string path, Type invokerType)
             {
                 Method = method;
                 Path = path;
                 InvokerType = invokerType;
-                Handler = null;
             }
 
             public string Method { get; }
             public string Path { get; }
-            public Type? InvokerType { get; }
-            public Func<IOctoRequest, Task<IOctoResponseProvider>>? Handler { get; }
+            public Type InvokerType { get; }
         }
     }
 }
