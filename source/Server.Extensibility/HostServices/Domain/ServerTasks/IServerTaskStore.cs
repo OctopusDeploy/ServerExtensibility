7bd7ba931b92f917e06b3e37f7accc3ca0ae02e7
slewis74 <slewis74@gmail.com>
2021-03-24 11:46:23 +1000

    moving more things to messagecontracts

--- source/Server.Extensibility/HostServices/Domain/ServerTasks/IServerTaskStore.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Octopus.Server.Extensibility.HostServices.Model.ServerTasks;
-
-namespace Octopus.Server.Extensibility.HostServices.Domain.ServerTasks
-{
-    public interface IServerTaskStore : IStore<IServerTask>
-    {
-    }
-}
--- source/Server.Extensibility/HostServices/Model/IdTinyType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Octopus.Server.Extensibility.HostServices.Model
-{
-    public interface IIdTinyType
-    {
-        string Value { get; }
-    }
-}
