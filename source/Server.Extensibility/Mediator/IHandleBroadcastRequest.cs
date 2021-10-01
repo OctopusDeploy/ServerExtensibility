f03e032ceb047f12f171aa8f3fcb623e7a61902e
Andrew Harcourt <andrew.harcourt@octopus.com>
2021-10-01 16:29:10 +1000

    Add the IHandleBroadcastRequest interface to allow extensions to respond to broadcast requests.

---
+++ source/Server.Extensibility/Mediator/IHandleBroadcastRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Octopus.Server.MessageContracts;
+
+namespace Octopus.Server.Extensibility.Mediator
+{
+    public interface IHandleBroadcastRequest<TRequest, TResponse>
+        where TRequest : IBroadcastRequest<TRequest, TResponse>
+        where TResponse : IResponse
+
+    {
+        IAsyncEnumerable<TResponse> Handle(IBroadcastRequest<TRequest, TResponse> request, CancellationToken cancellationToken);
+    }
+}
