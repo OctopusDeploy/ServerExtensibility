28da70bdf4461df6dbd1bbcb11544a31444b7f0f
John Simons <john.simons@octopus.com>
2021-10-19 10:07:00 +1000

    Missed this one

--- source/Server.Extensibility/Extensions/WorkItems/IWorkItemLinkMapper.cs
+++ source/Server.Extensibility/Extensions/WorkItems/IWorkItemLinkMapper.cs
@@ -9,7 +9,7 @@ namespace Octopus.Server.Extensibility.Extensions.WorkItems
     public interface IWorkItemLinkMapper
     {
         string CommentParser { get; }
-        bool IsEnabled { get; }
+        Task<bool> IsEnabled { get; }
 
         Task<IResultFromExtension<WorkItemLink[]>> Map(OctopusBuildInformation buildInformation);
     }
