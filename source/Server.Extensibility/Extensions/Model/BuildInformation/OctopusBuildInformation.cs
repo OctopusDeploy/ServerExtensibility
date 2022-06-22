fe2bc05a12cf2e7d7d4880eddd78c7491efbe394
Shannon Lewis <slewis74@gmail.com>
2022-06-22 16:26:19 +1000

    feat: build information v2, to include Committed values for commits

---
+++ source/Server.Extensibility/Extensions/Model/BuildInformation/OctopusBuildInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Octopus.Server.Extensibility.Extensions.Model.BuildInformation
+{
+    public class OctopusBuildInformation
+    {
+        public string BuildEnvironment { get; set; } = string.Empty;
+        public string BuildNumber { get; set; } = string.Empty;
+        public string? BuildUrl { get; set; }
+        public string? Branch { get; set; }
+        public string? VcsType { get; set; }
+        public string? VcsRoot { get; set; }
+        public string? VcsCommitNumber { get; set; }
+
+        public Commit[] Commits { get; set; } = new Commit[0];
+    }
+}
---
+++ source/Server.Extensibility/Extensions/WorkItems/WorkItemLink.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Octopus.Server.Extensibility.Extensions.WorkItems;
+
+public class WorkItemLink : IEquatable<WorkItemLink>, IComparable<WorkItemLink>
+{
+    static readonly Regex LastNumberRegex = new(@"(\d+)$", RegexOptions.Compiled);
+
+    public string Id { get; set; } = string.Empty;
+    public string LinkUrl { get; set; } = string.Empty;
+    public string Source { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public int CompareTo(WorkItemLink? other)
+    {
+        if (ReferenceEquals(this, other)) return 0;
+        if (ReferenceEquals(null, other)) return 1;
+        var thisIdMatch = LastNumberRegex.Match(Id);
+        var otherIdMatch = LastNumberRegex.Match(other.Id);
+        if (thisIdMatch.Success && otherIdMatch.Success)
+            return int.Parse(thisIdMatch.Groups[1].Value) - int.Parse(otherIdMatch.Groups[1].Value);
+        return string.Compare(Id, other.Id, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public bool Equals(WorkItemLink? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return string.Equals(Id, other.Id, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((WorkItemLink)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id != null ? Id.ToUpperInvariant().GetHashCode() : 0;
+    }
+}
